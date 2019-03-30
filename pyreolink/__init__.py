524e35a78f49ee8466281f9e9787871b25edf2db
rogatty <igor.rogatty@bethink.pl>
2019-03-30 14:52:01 +0100

    Clean up and add get_ir_lights function

--- pyreolink/__init__.py
+++ pyreolink/__init__.py
@@ -1,5 +1,8 @@
 # coding: utf-8
-"""Base Python Class file for Reolink camera."""
+"""
+Base Python Class file for Reolink camera
+Tested with Reolink C1 Pro
+"""
 import logging
 import requests
 import sys
@@ -13,6 +16,9 @@ logging.basicConfig(
 )
 
 _LOGGER = logging.getLogger(__name__)
+
+# Disable logging requested URLs to hide token from logs
+logging.getLogger('urllib3').setLevel(logging.WARNING)
 
 
 class PyReolink(object):
@@ -41,14 +47,13 @@ class PyReolink(object):
 
     def __repr__(self):
         """Object representation."""
-        return "<{0}: {1}>".format(self.__class__.__name__, self.__username)
+        return '<{0}: {1}>'.format(self.__class__.__name__, self.__username)
 
     def login(self):
         """Login to Reolink."""
-        _LOGGER.debug("Creating Reolink session")
+        _LOGGER.debug('Creating Reolink session')
         data = self.query(
-            {'cmd': 'Login'},
-            method='POST',
+            query_params={'cmd': 'Login'},
             body=[
                 {
                     'action': 0,
@@ -60,65 +65,45 @@ class PyReolink(object):
                         }
                     }
                 }
-            ])
+            ],
+            disable_logging=True)
 
         data = data[0]
 
         if isinstance(data, dict) and data.get('code') == 0:
             self.__token = data.get('value').get('Token').get('name')
-        else :
-            _LOGGER.error("Failed to authenticate")
+        else:
+            _LOGGER.error('Failed to authenticate')
 
-    def query(self,
-              query_params,
-              method='GET',
-              body=None,
-              retry=3):
-        """
-        Return a JSON object or raw session.
+    def get_ir_lights(self):
+        """Get IR lights state."""
+        _LOGGER.debug('Getting IR lights state')
+        data = self.query(
+            body=[
+                {
+                    'action': 1,
+                    'cmd': 'GetIrLights',
+                }
+            ])
 
-        :param query_params: Dictionary to use as query params
-        :param method: Specify the method GET, POST or PUT. Default is GET.
-        :param body: Dictionary to be appended on request.body
-        :param retry: Attempts to retry a query. Default is 3.
-        """
-        response = None
-        loop = 0
+        data = data[0]
 
-        while loop <= retry:
-            headers = {'Content-Type': 'application/json'}
-
-            if self.__token is not None:
-                query_params.update({'token': self.__token})
-
-            url = self.__base_url + '/cgi-bin/api.cgi?' + urlencode(query_params)
-
-            _LOGGER.debug("Querying %s on attempt: %s/%s", url, loop, retry)
-            _LOGGER.debug("Body: %s", body)
-            loop += 1
-
-            # define connection method
-            req = None
-
-            if method == 'GET':
-                req = self.session.get(url, headers=headers)
-            elif method == 'POST':
-                req = self.session.post(url, json=body, headers=headers)
-
-            if req and (req.status_code == 200):
-                response = req.json()
-                break
-
-        _LOGGER.debug("Response: %s", response)
-
-        return response
+        if isinstance(data, dict) and data.get('code') == 0:
+            state = data.get('value').get('IrLights').get('state')
+            _LOGGER.info('Successfully got IR lights state: %s', state)
+            return state
+        else:
+            _LOGGER.error('Failed to get IR lights state')
+            return None
 
     def set_ir_lights(self, state):
-        """Set IR lights."""
-        _LOGGER.debug("Setting IR lights to %s", state)
+        """
+        Set IR lights state
+
+        :param state: 'Auto' or 'Off'
+        """
+        _LOGGER.debug('Setting IR lights state to %s', state)
         data = self.query(
-            {'cmd': 'SetIrLights'},
-            method='POST',
             body=[
                 {
                     'action': 0,
@@ -134,17 +119,14 @@ class PyReolink(object):
         data = data[0]
 
         if isinstance(data, dict) and data.get('code') == 0:
-            _LOGGER.info("Successfully set IR lights to %s", state)
+            _LOGGER.info('Successfully set IR lights state to %s', state)
         else:
-            _LOGGER.error("Failed to set IR lights", state)
-
+            _LOGGER.error('Failed to set IR lights state', state)
 
     def goto_ptz_preset(self, preset_id):
         """Go to PTZ preset."""
-        _LOGGER.debug("Using PTZ preset with id %s", preset_id)
+        _LOGGER.debug('Using PTZ preset with id %s', preset_id)
         data = self.query(
-            {'cmd': 'PtzCtrl'},
-            method='POST',
             body=[
                 {
                     'action': 0,
@@ -161,7 +143,56 @@ class PyReolink(object):
         data = data[0]
 
         if isinstance(data, dict) and data.get('code') == 0:
-            _LOGGER.info("Successfully used PTZ preset with id %s", preset_id)
+            _LOGGER.info('Successfully used PTZ preset with id %s', preset_id)
         else:
-            _LOGGER.error("Failed to use PTZ preset with id %s", preset_id)
+            _LOGGER.error('Failed to use PTZ preset with id %s', preset_id)
 
+    def query(self,
+              query_params=None,
+              method='POST',
+              body=None,
+              disable_logging=False):
+        """
+        Return a JSON object or raw session.
+
+        :param query_params: Dictionary to use as query params
+        :param method: Specify the method GET or POST
+        :param body: Dictionary to be appended to request.body
+        :param disable_logging: Disable logging to e.g. hide credentials
+        """
+        if query_params is None:
+            query_params = {}
+
+        parsed_response = None
+        retry = 3
+        attempt = 1
+
+        if self.__token is not None:
+            query_params.update({'token': self.__token})
+
+        headers = {'Content-Type': 'application/json'}
+        url = self.__base_url + '/cgi-bin/api.cgi?' + urlencode(query_params)
+
+        while attempt <= retry:
+            if not disable_logging:
+                _LOGGER.debug('Sending request, attempt: %s/%s', attempt, retry)
+                _LOGGER.debug('Body: %s', body)
+
+            attempt += 1
+            response = None
+
+            if method == 'GET':
+                response = self.session.get(url, headers=headers)
+            elif method == 'POST':
+                response = self.session.post(url, json=body, headers=headers)
+
+            if response and (response.status_code == 200):
+                parsed_response = response.json()
+                break
+
+            # TODO handle errors, e.g. refresh session when expired
+
+        if not disable_logging:
+            _LOGGER.debug('Parsed response: %s', parsed_response)
+
+        return parsed_response
